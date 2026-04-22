b4137bd0c87d608be2adce2ad75accad1bef6e5c
Ztivy <ztivy1174@gmail.com>
2026-04-21 21:24:23 -0600

    se corrigio el la configuracion de vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+//import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
     open: true
   },
 })
-
+/*
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -19,3 +19,4 @@ export default defineConfig({
     },
   },
 })
+*/
